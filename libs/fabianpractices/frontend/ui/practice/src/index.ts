ed6001610f0a058bca97016595e7003796b74676
fabian rodriguez <103287298+FabianRG1990@users.noreply.github.com>
2024-10-16 23:51:32 -0600

    feat: export index

--- libs/fabianpractices/frontend/ui/practice/src/index.ts
+++ libs/fabianpractices/frontend/ui/practice/src/index.ts
@@ -5,8 +5,4 @@
 export * from './lib/practice.service';
 export * from './lib/practice.component';
 
-export * from './components/padre.component';
-
-export * from './components/hijoConDecorador/libs/fabianpractices/frontend/ui/prectice/src/components/hijoConDecorador.component';
-
-export * from './components/hijoConDecorador/ .component';
+export * from './components/padreConDecorador/padre.component';
